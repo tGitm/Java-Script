aa75c62f521696477b622f99e3f60d70c53d9601
tGitm <timrus99@gmail.com>
2020-04-12 16:50:05 +0200

    Challenge2 completed

---
+++ challenge2/script.js
@@ -0,0 +1,6 @@
+function generateCats() {
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
+    image.src = "cat-no-background-png-115733916033pvtnjtez0.png";
+    div.appendChild(image);
+}
